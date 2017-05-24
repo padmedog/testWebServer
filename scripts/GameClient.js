731ce5aafae8201b624959c4f0ef58ebe6b35d06
padmedog <fdelay8@gmail.com>
2017-05-24 11:25:24 -0400

    test change 14

---
+++ scripts/GameClient.js
@@ -0,0 +1,114 @@
+var GameClient = {
+	Connected: false,
+	Socket: null,
+	Connect: null,
+	SendPosition: null
+};
+
+var Players = [];
+
+var GamePlayer = (function () {
+    function GamePlayer(id) {
+        this.id = id;
+        var material = new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            specular: 0xffffff,
+            shininess: 5,
+            shading: THREE.FlatShading
+        });
+        this.pObj = new THREE.Mesh(new THREE.CylinderGeometry(4, 4, 12, 64, 48), material);
+		this.pObj.name = "player_" + this.id;
+		//this.name = this.pObj.name;
+    }
+    return GamePlayer;
+}());
+
+
+GameClient.Connect = function(address)
+{
+	GameClient.Socket = new WebSocket("ws://" + address);
+	GameClient.Socket.onopen = function(e) {
+		GameClient.Connected = true;
+		var elem = document.getElementById("result");
+		elem.innerHTML = "success";
+	};
+	GameClient.Socket.onclose = function(e) {
+		GameClient.Connected = false;
+	};
+	GameClient.Socket.onmessage = function(e) {
+		var data = e.data.split(' ');
+		switch(data[0])
+		{
+			case "pos":
+			{
+				var ind = GetPlayerIndById(parseInt(data[1]));
+				if(ind < 0)
+				{
+					break;
+				}
+				Players[ind].pObj.position.x = parseInt(data[2]);
+				Players[ind].pObj.position.z = parseInt(data[3]);
+				break;
+			}
+			case "add":
+			{
+				var pl = new GamePlayer(parseInt(data[1]));
+				pl.pObj.position.x = parseInt(data[2]);
+				pl.pObj.position.z = parseInt(data[3]);
+				Players.push(pl);
+				objectsToMove.push(pl.pObj);
+				scene.add(pl.pObj);
+				console.log("client " + pl.id + " connnected (ind " + GetPlayerIndById(pl.id) + ")");
+				break;
+			}
+			case "del":
+			{
+				var ind = GetPlayerIndById(parseInt(data[1]));
+				if(ind < 0)
+				{
+					break;
+				}
+				scene.remove(Players[ind].pObj.name);
+				
+				console.log("client " + Players[ind].id + " destroyed (ind " + ind + ")");
+				
+				var i = objectsToMove.indexOf(Players[ind].pObj);
+				if(i >= 0)
+				{
+					objectsToMove.splice(i, 1);
+				}
+				Players.splice(ind, 1);
+				break;
+			}
+		}
+	};
+};
+
+
+function GetPlayerIndById(id)
+{
+	for(var i = 0; i < Players.length; i++)
+	{
+		if(Players[i].id == id)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+function GetPlayerById(id)
+{
+	for(var i = 0; i < Players.length; i += 1)
+	{
+		if(Players[i].id == id)
+		{
+			return Players[i];
+		}
+	}
+	return null;
+}
+GameClient.SendPosition = function(position)
+{
+	GameClient.Socket.send("pos " + String(position.x) + " " + String(position.z));
+	console.log("sent position");
+};
